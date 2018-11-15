1b1ac380e015d5ce072c1adc6a806d9d3f602501
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-15 15:52:51 +0000

    test now has friendly messages in green

--- src/assert.js
+++ src/assert.js
@@ -5,19 +5,25 @@
 
   Expect.prototype.toEqual = function(expectation) {
     if (this.subject !== expectation) {
-      throw new Error(`FAIL: ${this.subject} does not equal "${expectation}"!`);
+      throw new Error(`FAIL: "${this.subject}" does not equal "${expectation}"!`,);
+    } else {
+      console.log(`%cHOORAY! "${this.subject}" is equal to "${expectation}", test passed!`, 'color: green');
     };
   };
 
   Expect.prototype.toHaveContent = function(expectation) {
     if (!this.subject.includes(expectation)) {
-      throw new Error(`FAIL: "${expectation}" not found in ${this.subject}!`);
+      throw new Error(`FAIL: "${expectation}" not found in "${this.subject}"!`);
+    } else {
+      console.log(`%cHOORAY! "${this.subject}" has content: "${expectation}", test passed!`, 'color: green')
     };
   };
 
   Expect.prototype.notToHaveContent = function(expectation) {
     if (this.subject.includes(expectation)) {
-      throw new Error(`FAIL: "${expectation}" found in ${this.subject}!`);
+      throw new Error(`FAIL: "${expectation}" found in "${this.subject}"!`);
+    } else {
+      console.log(`%cHOORAY! "${this.subject}" does not have content: "${expectation}", test passed!`, 'color: green')
     };
   };
 
